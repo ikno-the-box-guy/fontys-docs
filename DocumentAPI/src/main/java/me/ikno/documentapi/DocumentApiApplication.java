b620c34d5b0572871b89878b067bd0640379d8b9
Ikno <erwtje013@gmail.com>
2024-09-23 10:55:56 +0200

    Fixed port and cors issues

--- DocumentAPI/src/main/java/me/ikno/documentapi/DocumentApiApplication.java
+++ DocumentAPI/src/main/java/me/ikno/documentapi/DocumentApiApplication.java
@@ -1,14 +1,35 @@
 package me.ikno.documentapi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DocumentApiApplication {
+    @Value("${cors.origin}")
+    private String corsOrigin;
 
     public static void main(String[] args) {
         SpringApplication.run(DocumentApiApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        String corsOrigin = this.corsOrigin;
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .exposedHeaders("*")
+                        .allowedOrigins(corsOrigin)
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
