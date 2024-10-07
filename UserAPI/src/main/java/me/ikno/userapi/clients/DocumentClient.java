f6143ff9f1b822d4c6173a598522e6a6f834f9cb
Ikno <erwtje013@gmail.com>
2024-10-07 09:53:28 +0200

    Moved RestTemplate call to DocumentClient

---
+++ UserAPI/src/main/java/me/ikno/userapi/clients/DocumentClient.java
@@ -0,0 +1,45 @@
+package me.ikno.userapi.clients;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
+
+@Component
+public class DocumentClient {
+    @Value("${api.secret}")
+    private String apiSecret;
+
+    @Value("${api.doc-url}")
+    private String docApiUrl;
+
+    private final RestTemplate restTemplate;
+
+    public DocumentClient() {
+        this.restTemplate = new RestTemplate();
+    }
+
+    public Optional<String> createRootDirectory(int userId) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cookie", "token=" + apiSecret);
+
+        try {
+            ResponseEntity<String> rootResponse = restTemplate.exchange(
+                    docApiUrl + "/directories/root/" + userId,
+                    HttpMethod.POST,
+                    new HttpEntity<>(headers),
+                    String.class
+            );
+
+            return Optional.ofNullable(rootResponse.getBody());
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+}
--- UserAPI/src/main/java/me/ikno/userapi/services/AuthorizationService.java
+++ UserAPI/src/main/java/me/ikno/userapi/services/AuthorizationService.java
@@ -1,5 +1,6 @@
 package me.ikno.userapi.services;
 
+import me.ikno.userapi.clients.DocumentClient;
 import me.ikno.userapi.exceptions.EmailTakenException;
 import me.ikno.userapi.exceptions.InvalidCredentialsException;
 import me.ikno.userapi.models.LoginResult;
@@ -7,37 +8,28 @@ import me.ikno.userapi.models.RegisterResult;
 import me.ikno.userapi.models.UserModel;
 import me.ikno.userapi.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Optional;
 
 @Service
 public class AuthorizationService {
+    private final DocumentClient documentClient;
     @Value("${password.pepper}")
     private String pepper;
-
-    @Value("${api.secret}")
-    private String apiSecret;
-
-    @Value("${api.doc-url}")
-    private String docApiUrl;
 
     private final JwtService jwtService;
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder encoder;
 
-    public AuthorizationService(JwtService jwtService, UserRepository userRepository) {
+    public AuthorizationService(JwtService jwtService, UserRepository userRepository, DocumentClient documentClient) {
         this.jwtService = jwtService;
         this.userRepository = userRepository;
 
         encoder = new BCryptPasswordEncoder(10);
+        this.documentClient = documentClient;
     }
 
     public LoginResult login(String email, String password) {
@@ -77,25 +69,13 @@ public class AuthorizationService {
             throw new EmailTakenException("Email " + email + " has already been taken");
         }
 
-        RestTemplate restTemplate = new RestTemplate();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Cookie", "token=" + apiSecret);
-
-        ResponseEntity<String> rootResponse = restTemplate.exchange(
-                docApiUrl + "/directories/root/" + newUser.getId(),
-                HttpMethod.POST,
-                new HttpEntity<>(headers),
-                String.class
-        );
-
-        String rootDirectoryId = rootResponse.getBody();
-        if(rootDirectoryId == null) {
+        Optional<String> rootDirectoryId = documentClient.createRootDirectory(newUser.getId());
+        if(rootDirectoryId.isEmpty()) {
             userRepository.delete(newUser);
             throw new RuntimeException("Failed to create root directory");
         }
 
-        newUser.setRootDirectoryId(rootDirectoryId);
+        newUser.setRootDirectoryId(rootDirectoryId.get());
         userRepository.save(newUser);
 
         return new RegisterResult(newUser);
